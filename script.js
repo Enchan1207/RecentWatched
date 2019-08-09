7b89188e6333a2569a1077def08e09c1905f7c9c
Enchan <enchant0130@gmail.com>
2019-08-09 23:22:58 +0900

    add watched list

--- script.js
+++ script.js
@@ -32,8 +32,39 @@ window.onload  = function(){
 
     //--ホームページならlocalstorageから呼び出してリストを生成し、トップバーの下に追加
     else{
+        //--ラベルを作る
+        let label_cell = document.createElement("tr");
+        let label_td = document.createElement("td");
+
+        label_td.setAttribute("colspan", "3");
+        label_td.setAttribute("class", "lineb");
+        label_td.setAttribute("style", "text-align:center;font-size:1.2em;");
+
+        label_cell.appendChild(label_td);
+        label_td.innerHTML = "最近閲覧した商品 " + label_td.innerHTML;
+
+        //--リストを作る
+        let list_cell = document.createElement("tr");
+        let list_td = document.createElement("td");
+        let list_div = document.createElement("div");
+
+        list_td.setAttribute("colspan", "3");
+        list_div.setAttribute("style", "height:100px;test-align:center;background-color:#fff;margin:0px 0px 10px 0px;");
+        list_div.setAttribute("id", "recentwatched"); //これで、selectorに#recentwatchedを渡すだけで一意に定められる
+
+        list_td.appendChild(list_div);
+        list_cell.appendChild(list_td);
+
+        //--貼り付け位置を取得してappend
+        let target = document.querySelector("div#header table tbody");
+        target.appendChild(label_cell);
+        target.appendChild(list_cell);
 
     }
+}
 
+//--「最近閲覧した商品」リストの表示を更新
+function listupdate(page) {
 
+    
 }
